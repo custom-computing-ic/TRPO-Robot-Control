bfa28a3d206963e749adbfecd4bbf52e5dd99cfa
ss13412 <sjshao09@gmail.com>
2018-07-08 15:07:05 +0100

    Fix Latency Issue in Trd Layer CPP

--- src/TRPO_Lightweight_FPGA.c
+++ src/TRPO_Lightweight_FPGA.c
@@ -421,7 +421,7 @@ double TRPO_Lightweight_FPGA (TRPOparam param, const int NumIter, const size_t N
     // Remarks: Here we assume 4 layers
     size_t MaxBlkDim0Dim2     = ( (BlockDim[0]>BlockDim[2]) ? BlockDim[0] : BlockDim[2] ) + EvenLayerCompExtraLatency;
     size_t FwdCyclesPerSample = BlockDim[0] + (BlockDim[1]-1)*MaxBlkDim0Dim2 + BlockDim[2]*BlockDim[3];
-    size_t BwdCyclesPerSample = BlockDim[1]*MaxBlkDim0Dim2 + BlockDim[2]*BlockDim[3];
+    size_t BwdCyclesPerSample = BlockDim[1]*MaxBlkDim0Dim2 + (BlockDim[2]+EvenLayerCompExtraLatency)*BlockDim[3];
     size_t CyclesPerSample    = (FwdCyclesPerSample>BwdCyclesPerSample) ? FwdCyclesPerSample : BwdCyclesPerSample;
     size_t PropCyclesTotal    = CyclesPerSample * (NumSamples + 1);
 
